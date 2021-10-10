46bd63fd13856449c66cc5bc44a3ed6c70eba5bc
MisterPot <english.man.spy@gmail.com>
2021-10-10 20:27:15 +0300

    Fixed attribute 'from_end' in ls.py

--- ls.py
+++ ls.py
@@ -2,35 +2,16 @@ class list(list):
 
     def replace(self, from_, to_, max_replaces=1, from_end=False):
 
-        def fr_start():
-            for i in range(0, max_replaces):
-                index = self.index(from_)
-                self[index] = to_
-            return self
-
-        def fr_end():
+        if from_end:
             self.reverse()
-            types = [str, list, set, frozenset]
 
-            if type(from_) in types:
-                fr_rev = from_[::-1]
-            else:
-                fr_rev = from_
+        for i in range(0, max_replaces):
+            index = self.index(from_)
+            self[index] = to_
 
-            if type(to_) in types:
-                to_rev = to_[::-1]
-            else:
-                to_rev = to_
-
-            for i in range(0, max_replaces):
-                index = self.index(fr_rev)
-                self[index] = to_rev
-
+        if from_end:
             return self.reverse()
-
-        d = {False: fr_start,
-             True: fr_end}
-        return d[from_end]()
+        return self
 
     def stringify(self):
         st = ''
